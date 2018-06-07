66861b468f1cc7633f4cf4fcf4f0e27972fb0957
Kenny Zhang <busstopbench@gmail.com>
2018-06-06 21:22:00 -0500

    Create method to read .mat files into adjacency matrices

---
+++ graph_utils.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+
+def create_adj_mat(filepath):
+    '''Given a filepath, reads the contents and returns an adjacency matrix.'''
+    adj_mat = []
+    with open(filepath, 'r') as file:
+        # Read all lines from the file...
+        lines = [line.strip() for line in file]
+
+        for line in lines:
+            if len(line) > 0:
+                adj_mat.append([int(x) for x in line.split(' ')])
+    
+    return adj_mat
+
+
+if __name__ == '__main__':
+    pprint(create_adj_mat('./trees/cross.mat'))
+    pprint(create_adj_mat('./trees/ethane.mat'))    
+
+        
