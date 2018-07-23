5a852c5ba09547b35023de6d8d8ad77cd1ff0b42
Kenny Zhang <busstopbench@gmail.com>
2018-07-23 19:43:39 +0000

    Fix burning script for md algorithm

--- notebooks/burn_all_trees.py
+++ notebooks/burn_all_trees.py
@@ -17,13 +17,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
     alg = args.alg
     
+    print("Using algorithm", alg)
+    
     n = 2
     while True:   
+        print("\n" + "*"*20)
         print("Order:", n, flush=True)
+        print("*"*20)
         
         # Burn all non-isomorphic trees of order n
         trees = nx.generators.nonisomorphic_trees(n)
+        num_trees = 0
         for tree in trees:
+            upper_bound = math.ceil(math.sqrt(tree.order()))
+            
             if alg == 'frl':
                 # Fixed Root, Most Leaves
                 burning_sequence = burn_most_leaves_fixed_root(tree)
@@ -32,16 +39,22 @@ if __name__ == '__main__':
                 burning_sequence = burn_most_leaves_reroot(tree)
             elif alg == 'md':
                 # Max Depth
-                burning_sequence = burn_tree(tree)
+                burning_sequence, marked = burn_tree(tree)
             else:
                 # Re-Root, Most Removable Nodes
                 burning_sequence = burn_most_removed(tree)
     
-            upper_bound = math.ceil(math.sqrt(tree.order()))
-
             if len(burning_sequence) > upper_bound:
                 print('n={0:2d} | b(G)<={1:2d} | ceil(sqrt(n))={2:2d}'.format(tree.order(),
                                                                               len(burning_sequence),                  
                                                                               math.ceil(math.sqrt(tree.order()))), flush=True)
+                print("Nodes:", tree.nodes())
+                print("Edges:", tree.edges())
+                print()
+                
+            num_trees += 1
+        
+        print(tree)
+        print("There are {} trees of order {}".format(num_trees, n))
         n += 1
     
